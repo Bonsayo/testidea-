4a68d8bd838994b4963459294ee8b47a97bfaa62
Bonsayo <bonsayo@github.com>
2026-07-01 09:30:18 +0300

    Remove scratch files

--- check_db.cjs
+++
@@ -1,9 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('./data/cyber_basketball.db');
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
-console.log('Tables:', tables.map(t => t.name).join(', '));
-for (const t of tables) {
-  const count = db.prepare(`SELECT COUNT(*) as c FROM "${t.name}"`).get();
-  console.log(t.name + ': ' + count.c + ' rows');
-}
-db.close();
